3492bf77bb635b4e35283098efe33c1736271d64
HectorSalo <43446563+HectorSalo@users.noreply.github.com>
2020-07-28 21:20:14 -0400

    V3.0.1 Implementacion Ajustes Activity/Bloqueo

---
+++ app/src/main/java/com/skysam/hchirinos/myfinances/principal/BloqueoActivity.java
@@ -0,0 +1,16 @@
+package com.skysam.hchirinos.myfinances.principal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.skysam.hchirinos.myfinances.R;
+
+public class BloqueoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bloqueo);
+    }
+}
---
+++ app/src/main/java/com/skysam/hchirinos/myfinances/principal/LaunchActivity.java
@@ -0,0 +1,38 @@
+package com.skysam.hchirinos.myfinances.principal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.skysam.hchirinos.myfinances.R;
+import com.skysam.hchirinos.myfinances.inicioSesion.InicSesionActivity;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        final LottieAnimationView lottieAnimationView = findViewById(R.id.lottieAnimationView);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                lottieAnimationView.cancelAnimation();
+            }
+        }, 4000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), InicSesionActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4500);
+    }
+}
