78d1d41339ac6bae55410ae58eade9dd6668f729
HectorSalo <43446563+HectorSalo@users.noreply.github.com>
2020-08-02 23:37:54 -0400

    V3.0.2 Implementacion Ajustes Activity/Bloqueo Kotlin

---
+++ app/src/main/java/com/skysam/hchirinos/myfinances/principal/BloqueoActivity.kt
@@ -0,0 +1,12 @@
+package com.skysam.hchirinos.myfinances.principal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.skysam.hchirinos.myfinances.R
+
+class BloqueoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bloqueo)
+    }
+}
