4c3edbf59afb11bd1c307cd9027d692aaac93913
HectorSalo <43446563+HectorSalo@users.noreply.github.com>
2021-05-16 21:49:34 -0400

    V7.0.1 MainViewModel

--- app/src/main/java/com/skysam/hchirinos/myfinances/database/repositories/IncomesRepository.kt
+++ app/src/main/java/com/skysam/hchirinos/myfinances/database/repositories/IncomesRepository.kt
@@ -8,6 +8,7 @@ import com.google.firebase.firestore.Query
 import com.skysam.hchirinos.myfinances.common.model.constructores.IngresosGastosConstructor
 import com.skysam.hchirinos.myfinances.common.model.firebase.Auth
 import com.skysam.hchirinos.myfinances.common.utils.Constants
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 
@@ -34,9 +35,34 @@ object IncomesRepository {
 
                         val incomes: MutableList<IngresosGastosConstructor> = mutableListOf()
                         for (doc in value!!) {
-
+                            val ingreso = IngresosGastosConstructor()
+                            ingreso.idIngreso = doc.id
+                            ingreso.concepto = doc.getString(Constants.BD_CONCEPTO)
+                            ingreso.monto = doc.getDouble(Constants.BD_MONTO)!!
+                            ingreso.isDolar = doc.getBoolean(Constants.BD_DOLAR)!!
+                            val activo = doc.getBoolean(Constants.BD_MES_ACTIVO)
+                            if (activo == null) {
+                                ingreso.isMesActivo = true
+                            } else {
+                                ingreso.isMesActivo = activo
+                            }
+                            val tipoFrecuencia = doc.getString(Constants.BD_TIPO_FRECUENCIA)
+                            if (tipoFrecuencia != null) {
+                                val duracionFrecuencia = doc.getDouble(Constants.BD_DURACION_FRECUENCIA)!!
+                                val duracionFrecuenciaInt = duracionFrecuencia.toInt()
+                                ingreso.duracionFrecuencia = duracionFrecuenciaInt
+                                ingreso.fechaIncial = doc.getDate(Constants.BD_FECHA_INCIAL)!!
+                                ingreso.tipoFrecuencia = doc.getString(Constants.BD_TIPO_FRECUENCIA)
+                                ingreso.fechaFinal = doc.getDate(Constants.BD_FECHA_FINAL)
+                            } else {
+                                ingreso.tipoFrecuencia = null
+                                ingreso.fechaFinal = null
+                            }
+                            incomes.add(ingreso)
                         }
+                        offer(incomes)
                     }
+            awaitClose { request.remove() }
         }
     }
 }
