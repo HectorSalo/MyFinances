5e360c352b499537de1050bfd8fca940cbd262d0
Prueba <hectorsalomonchirinos@gmial.com>
2021-06-11 15:49:12 -0400

    V7.1 Ajuste en FAB inicio

--- app/src/main/java/com/skysam/hchirinos/myfinances/database/repositories/LoansRespoitory.kt
+++
@@ -1,7 +0,0 @@
-package com.skysam.hchirinos.myfinances.database.repositories
-
-/**
- * Created by Hector Chirinos (Home) on 4/5/2021.
- */
-object LoansRespoitory {
-}
--- app/src/main/java/com/skysam/hchirinos/myfinances/database/sharedPreferences/SharedPref.kt
+++
@@ -1,7 +0,0 @@
-package com.skysam.hchirinos.myfinances.database.sharedPreferences
-
-/**
- * Created by Hector Chirinos (Home) on 4/5/2021.
- */
-object SharedPref {
-}
